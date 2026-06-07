6073e9deac971d3f8b45a3a29f320ea62e181df2
dev-ahad-ali <ahadali.dev@gmail.com>
2026-06-07 19:02:31 +0600

    Update: added gmt

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,11 +67,24 @@ export default function RootLayout({
             alt=""
           />
         </noscript>
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-NVB42XZB');</script>
       </head>
       <body className="font-sans antialiased">
         {children}
         <Toaster />
         {process.env.NODE_ENV === 'production' && <Analytics />}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NVB42XZB"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
       </body>
     </html>
   )
