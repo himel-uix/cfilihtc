9c5f7097dcab388371f6da155f075794895ba705
rafim699956 <rafim699956@gmail.com>
2026-06-08 18:13:01 +0600

    whatsapp button size increage

--- app/page.tsx
+++ app/page.tsx
@@ -59,11 +59,11 @@ export default function HomePage() {
             </main>
             {/* sociel */}
             <div className="flex flex-col gap-1 fixed bottom-3 right-3 md:bottom-8 md:right-8 z-999">
-                <Link target="_blank" href="https://wa.me/8801345245494" className="size-13.5 rounded-full cursor-pointer">
-                    <Image className="w-full" src={whatsappIcon} alt="whatsapp logo" width={54} height={54} />
+                <Link target="_blank" href="https://wa.me/8801345245494" className="size-20 rounded-full">
+                    <Image className="w-full" src={whatsappIcon} alt="whatsapp logo" width={80} height={80} />
                 </Link>
-                <button onClick={() => setIsPopUpOpen(true)} className="size-13.5 rounded-full cursor-pointer">
-                    <Image className="w-full" src={privacyPolicyIcon} alt="privacy policy" width={54} height={54} />
+                <button onClick={() => setIsPopUpOpen(true)} className="size-20 rounded-full cursor-pointer">
+                    <Image className="w-full" src={privacyPolicyIcon} alt="privacy policy" width={80} height={80} />
                 </button>
             </div>
             {/* sociel */}
