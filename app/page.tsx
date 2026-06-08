7954d753b6df96f2174ce9f351ad9dca9672eb22
dev-ahad-ali <ahadali.dev@gmail.com>
2026-06-08 14:32:50 +0600

    Update: button size changed

--- app/page.tsx
+++ app/page.tsx
@@ -17,8 +17,8 @@ import Offer from "@/components/landing/Offer";
 import FinalCta from "@/components/landing/FinalCta";
 import Review from "@/components/landing/Review";
 import Support from "@/components/landing/Support";
-import whatsappIcon from "@/public/icons/whatsapp-icon.png"
-import privacyPolicyIcon from "@/public/icons/privacy-policy.png"
+import whatsappIcon from "@/public/icons/whatsapp-icon.png";
+import privacyPolicyIcon from "@/public/icons/privacy-policy.png";
 import NotificationPopup from "@/components/landing/NotificationPopup";
 import PolicyPopup from "@/components/landing/PolicyPopup";
 import { useState } from "react";
@@ -35,7 +35,7 @@ export default function HomePage() {
     const [ispopupOpen, setIsPopUpOpen] = useState(false);
     const handleClosePopup = () => {
         setIsPopUpOpen(!ispopupOpen);
-    }
+    };
 
     return (
         <>
@@ -59,11 +59,30 @@ export default function HomePage() {
             </main>
             {/* sociel */}
             <div className="flex flex-col gap-1 fixed bottom-3 right-3 md:bottom-8 md:right-8 z-999">
-                <Link target="_blank" href="https://wa.me/8801345245494" className="size-13.5 rounded-full cursor-pointer">
-                    <Image className="w-full" src={whatsappIcon} alt="whatsapp logo" width={54} height={54} />
+                <Link
+                    target="_blank"
+                    href="https://wa.me/8801345245494"
+                    className="size-20 rounded-full cursor-pointer"
+                >
+                    <Image
+                        className="w-full"
+                        src={whatsappIcon}
+                        alt="whatsapp logo"
+                        width={54}
+                        height={54}
+                    />
                 </Link>
-                <button onClick={() => setIsPopUpOpen(true)} className="size-13.5 rounded-full cursor-pointer">
-                    <Image className="w-full" src={privacyPolicyIcon} alt="privacy policy" width={54} height={54} />
+                <button
+                    onClick={() => setIsPopUpOpen(true)}
+                    className="size-20 rounded-full cursor-pointer"
+                >
+                    <Image
+                        className="w-full"
+                        src={privacyPolicyIcon}
+                        alt="privacy policy"
+                        width={54}
+                        height={54}
+                    />
                 </button>
             </div>
             {/* sociel */}
